38f2211edc6e3cbeba86491f4ccb15d36ab1d32f
bek2518 <bereketb20@hotmail.com>
2023-05-11 16:34:08 +0300

    Trial with removed request handler

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,11 +5,26 @@ After every 10 lines and/or a keyboard interruption prints the
 statistics from the begining
 '''
 import sys
+import signal
 
 
 count = 0
 file_size = 0
 status_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+
+def interrupt_handler(signal, frame):
+    '''
+    Function that handles if keyboard interrupt happens
+    '''
+    print('File size: {}'.format(file_size))
+    for key, value in sorted(status_count.items()):
+        if status_count[key] != 0:
+            print('{}: {}'.format(key, value))
+
+
+signal.signal(signal.SIGINT, interrupt_handler)
+
 
 for line in sys.stdin:
     try:
@@ -18,10 +33,6 @@ for line in sys.stdin:
 
         size = int(size)
         status_code = int(status_code)
-        request = request_method + ' ' + project + ' ' + request_type
-
-        if request != '"GET /projects/260 HTTP/1.1"':
-            continue
 
     except Exception:
         continue
