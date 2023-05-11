344270b15719ac7609b5ac991d22f30cec25f498
bek2518 <bereketb20@hotmail.com>
2023-05-11 17:08:12 +0300

    Trial reverting back

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,25 +5,11 @@ After every 10 lines and/or a keyboard interruption prints the
 statistics from the begining
 '''
 import sys
-import signal
 
 
 count = 0
 file_size = 0
 status_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
-
-def interrupt_handler(signal, frame):
-    '''
-    Function that handles if keyboard interrupt happens
-    '''
-    print('File size: {}'.format(file_size))
-    for key, value in sorted(status_count.items()):
-        if status_count[key] != 0:
-            print('{}: {}'.format(key, value))
-
-
-signal.signal(signal.SIGINT, interrupt_handler)
 
 
 try:
