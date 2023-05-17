bb7f123a5e780e2ef3071fa8b61292aeebcd08d8
bek2518 <bereketb20@hotmail.com>
2023-05-17 18:33:37 +0300

    Initial trial

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,47 +9,13 @@ def validUTF8(data):
     Method that recieves list of intgers data, which each integer represent
     1 byte of data and determines if valid UTF-8
     '''
-    if len(data) == 0:
-        return False
-    if max(data) > 255 or min(data) < 0:
-        return False
-
-    binary_data = []
+    response_list = []
     for i in range(len(data)):
-        if type(data[i]) != int:
+        if (data[i] > 255 or type(data[i]) != int):
+            response_list.append(False)
+        else:
+            response_list.append(True)
+    for i in range(len(response_list)):
+        if response_list[i] is False:
             return False
-        binary_data.append(bin(data[i])[2:].zfill(8))
-
-    i = 0
-    while i < len(binary_data):
-        try:
-            if binary_data[i][0] == '0':
-                i += 1
-                continue
-            elif binary_data[i][0:2] == '10':
-                return False
-
-            elif binary_data[i][0:3] == '110':
-                if (binary_data[i + 1][0:2] != '10'):
-                    return False
-                i += 2
-                continue
-
-            elif binary_data[i][0:4] == '1110':
-                if (binary_data[i + 1][0:2] != '10' and
-                   binary_data[i + 2][0:2] != '10'):
-                    return False
-                i += 3
-                continue
-
-            elif binary_data[i][0:5] == '11110':
-                if (binary_data[i + 1][0:2] != '10' and
-                   binary_data[i + 2][0:2] != '10' and
-                   binary_data[i + 3][0:2] != '10'):
-                    return False
-                i += 4
-                continue
-        except Exception:
-            return False
-
     return True
