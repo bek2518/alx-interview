eac77a549420100f5e4e30d0cbdbfe6bb7d5eff5
bek2518 <bereketb20@hotmail.com>
2023-05-18 12:25:31 +0300

    Pycodestyle edit and trial

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,41 +11,38 @@ def validUTF8(data):
     '''
     if not data:
         return False
-    
-    if max(data) > 247:
+
+    if max(data) > 255:
         return False
 
     i = 0
     while i < len(data):
-        try:
-            if data[i] < 128:
-                i += 1
+        if data[i] < 128:
+            i += 1
 
-            elif 128 <= data[i] < 192:
-                return False
-
-            elif 192 <= data[i] <= 223:
-                if (data[i] == 192 and 128 <= data[i + 1] < 192) or \
-                        (data[i] == 193 and 128 <= data[i + 1] < 192):
-                    return False
-                if not (128 <= data[i + 1] < 192):
-                    return False
-                i += 2
-
-            elif 224 <= data[i] <= 239:
-                if not (128 <= data[i + 1] < 192 and
-                        128 <= data[i + 2] < 192):
-                    return False
-                i += 3
-
-            elif 240 <= data[i] <= 247:
-                if not (128 <= data[i + 1] < 192 and
-                        128 <= data[i + 2] < 192 and
-                        128 <= data[i + 3] < 192):
-                    return False
-                i += 4
-
-        except Exception:
+        elif 128 <= data[i] < 192:
             return False
+
+        elif 192 <= data[i] <= 223:
+            if (data[i] == 192 and 128 <= data[i + 1] < 192) or \
+                    (data[i] == 193 and 128 <= data[i + 1] < 192):
+                return False
+            if not (128 <= data[i + 1] < 192):
+                return False
+            i += 2
+
+        elif 224 <= data[i] <= 239:
+            if not (128 <= data[i + 1] < 192 and
+                    128 <= data[i + 2] < 192):
+                return False
+            i += 3
+
+        elif 240 <= data[i] <= 247:
+            if not (128 <= data[i + 1] < 192 and
+                    128 <= data[i + 2] < 192 and
+                    128 <= data[i + 3] < 192):
+                return False
+            i += 4
+
 
     return True
