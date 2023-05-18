bc1161f7a44fb2330dc65b71596796fc394e206f
bek2518 <bereketb20@hotmail.com>
2023-05-18 12:28:56 +0300

    Returned try/except block

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -17,31 +17,35 @@ def validUTF8(data):
 
     i = 0
     while i < len(data):
-        if data[i] < 128:
-            i += 1
+        try:
+            if data[i] < 128:
+                i += 1
 
-        elif 128 <= data[i] < 192:
+            elif 128 <= data[i] < 192:
+                return False
+
+            elif 192 <= data[i] <= 223:
+                if (data[i] == 192 and 128 <= data[i + 1] < 192) or \
+                        (data[i] == 193 and 128 <= data[i + 1] < 192):
+                    return False
+                if not (128 <= data[i + 1] < 192):
+                    return False
+                i += 2
+
+            elif 224 <= data[i] <= 239:
+                if not (128 <= data[i + 1] < 192 and
+                        128 <= data[i + 2] < 192):
+                    return False
+                i += 3
+
+            elif 240 <= data[i] <= 247:
+                if not (128 <= data[i + 1] < 192 and
+                        128 <= data[i + 2] < 192 and
+                        128 <= data[i + 3] < 192):
+                    return False
+                i += 4
+
+        except Exception:
             return False
-
-        elif 192 <= data[i] <= 223:
-            if (data[i] == 192 and 128 <= data[i + 1] < 192) or \
-                    (data[i] == 193 and 128 <= data[i + 1] < 192):
-                return False
-            if not (128 <= data[i + 1] < 192):
-                return False
-            i += 2
-
-        elif 224 <= data[i] <= 239:
-            if not (128 <= data[i + 1] < 192 and
-                    128 <= data[i + 2] < 192):
-                return False
-            i += 3
-
-        elif 240 <= data[i] <= 247:
-            if not (128 <= data[i + 1] < 192 and
-                    128 <= data[i + 2] < 192 and
-                    128 <= data[i + 3] < 192):
-                return False
-            i += 4
 
     return True
