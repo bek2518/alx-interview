a6afc9b1d9febe96bf541ce48df8af8b24f2a154
bek2518 <bereketb20@hotmail.com>
2023-05-18 11:47:30 +0300

    Added handler for overlong encoding and pycode edit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -22,20 +22,23 @@ def validUTF8(data):
                 return False
 
             elif 192 <= data[i] <= 223:
+                if (data[i] == 192 and 128 <= data[i + 1] < 192) or \
+                        (data[i] == 193 and 128 <= data[i + 1] < 192):
+                    return False
                 if not (128 <= data[i + 1] < 192):
                     return False
                 i += 2
 
             elif 224 <= data[i] <= 239:
                 if not (128 <= data[i + 1] < 192 and
-                    128 <= data[i + 2] < 192):
+                        128 <= data[i + 2] < 192):
                     return False
                 i += 3
 
             elif 240 <= data[i] <= 255:
                 if not (128 <= data[i + 1] < 192 and
-                    128 <= data[i + 2] < 192 and
-                    128 <= data[i + 3] < 192):
+                        128 <= data[i + 2] < 192 and
+                        128 <= data[i + 3] < 192):
                     return False
                 i += 4
 
