3b3668f2a9d932ef0d54e21fe69150bb51871fb6
bek2518 <bereketb20@hotmail.com>
2023-06-22 10:17:40 +0300

    Added overlap counter to handle section with multiple overlap

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,12 +11,27 @@ def island_perimeter(grid):
     grid as an argument
     '''
     counter = 0
+    overlapCounterList = []
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
+                overlapCounter = 0
+                if (i > 0):
+                    if grid[i - 1][j] == 1:
+                        overlapCounter += 1
+                if (i < (len(grid) - 1)):
+                    if grid[i + 1][j] == 1:
+                        overlapCounter += 1
+                if (j > 0):
+                    if grid[i][j - 1] == 1:
+                        overlapCounter += 1
+                if (j < (len(grid[i]) - 1)):
+                    if grid[i][j + 1] == 1:
+                        overlapCounter += 1
                 counter += 1
-
-    overlap = (counter - 1) * 2
-    totalSides = counter * 4
-    perimeter = totalSides - overlap
-    return (perimeter)
+                overlapCounterList.append(overlapCounter)
+            overlapCounter = 0
+    overlap = sum(overlapCounterList)
+    totalArea = counter * 4
+    perimeter = totalArea - overlap
+    return perimeter
